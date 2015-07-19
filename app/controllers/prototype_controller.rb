d298080bd83f4a517ff5cc721ef632536739335a
FunnyQ <funnyq@funnyq.com>
2015-07-19 11:38:54 +0800

    [clean] refactor template layouts
      - adding 'landing' layout
      - move 'header' and 'footer' partials to application layout
      - using landing layout in landing page, signup and signin pages
      - change footer position to 'fixed'

--- app/controllers/prototype_controller.rb
+++ app/controllers/prototype_controller.rb
@@ -1,12 +1,15 @@
 class PrototypeController < ApplicationController
 
   def index
+    render layout: 'landing'
   end
 
   def sign_in
+    render layout: 'landing'
   end
 
   def sign_up
+    render layout: 'landing'
   end
 
   def main
