ff1e4614ebb59a997010c6b458977aa40ca8576b
mepsmike <mepsmike@gmail.com>
2015-07-22 16:40:26 +0800

    [add]set geocoder

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,7 @@ class Event < ActiveRecord::Base
   accepts_nested_attributes_for :photos
 
   as_enum :category, event: 0, dm: 1
+
+  geocoded_by :address
+  after_validation :geocode, if => :address_changed?
 end
