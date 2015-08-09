7c408cbd0b32b75831e8944151f7353bdf4845f9
FunnyQ <funnyq@funnyq.com>
2015-08-09 23:54:08 +0800

    [hotfix] adding ckeditor config into precompile list

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,6 @@ module Joinfuns
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.precompile += %w(ckeditor/*)
   end
 end
