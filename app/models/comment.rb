7b360fe71eebabc23dde4a1bff5702189be7637b
mepsmike <mepsmike@gmail.com>
2015-08-14 01:08:25 +0800

    [revise]move comment time method to model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,14 @@ class Comment < ActiveRecord::Base
   belongs_to :event, :counter_cache => true
   belongs_to :user
 
+  def show_time(time)
+
+    if time > Time.now.beginning_of_day
+      time_ago_in_words(time)
+    else
+      time.strftime("%b %d, %Y")
+    end
+
+  end
+
 end
