b4fab02af6ab32f8f3f8563e5738ec3ee1aa369c
SeanWu <shengfongwu@gmail.com>
2015-09-23 01:08:09 +0800

    [add] add_city_price_column to event

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150909084927) do
+ActiveRecord::Schema.define(version: 20150921145911) do
 
   create_table "collects", force: :cascade do |t|
     t.datetime "created_at",           null: false
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20150909084927) do
     t.string   "cover_content_type", limit: 255
     t.integer  "cover_file_size",    limit: 4
     t.datetime "cover_updated_at"
+    t.string   "citytalk_price",     limit: 255
   end
 
   create_table "hosts", force: :cascade do |t|
